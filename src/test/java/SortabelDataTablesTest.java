a3362997cdda2a04728b7b57fa1aaff95e92872b
Ppolyak <pasha_polyakov8@mail.ru>
2022-10-18 17:44:08 +0300

    Tables Task

---
+++ src/test/java/SortabelDataTablesTest.java
@@ -0,0 +1,49 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class SortabelDataTablesTest {
+    private WebDriver driver;
+    private final String pageUrl = "http://the-internet.herokuapp.com/tables";
+    private final By firstCell = By.xpath("//table//tr[2]//td[5]");
+    private final By secondCell = By.xpath("//table//tr[4]//td[3]");
+    private final By thirdCell = By.xpath("//table//tr[3]//td[4]");
+    private final String firstCellText = "http://www.frank.com";
+    private final String secondCellText = "tconway@earthlink.net";
+    private final String thirdCellText = "$100.00";
+
+    @BeforeClass
+    public void openBrowser() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void checkFirstCellTest(){
+        driver.get(pageUrl);
+        String cellText = driver.findElement(firstCell).getText();
+        Assert.assertEquals(firstCellText,cellText);
+    }
+
+    @Test
+    public void checkSecondCellTest(){
+        driver.get(pageUrl);
+        String cellText = driver.findElement(secondCell).getText();
+        Assert.assertEquals(secondCellText,cellText);
+    }
+
+    @Test
+    public void checkThirdCellTest(){
+        driver.get(pageUrl);
+        String cellText = driver.findElement(thirdCell).getText();
+        Assert.assertEquals(thirdCellText,cellText);
+    }
+
+    @AfterClass
+    public void closeBrowser(){
+        driver.quit();}
+}
